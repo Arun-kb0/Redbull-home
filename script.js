9ddbc822173ac8edd9e7c96755fb601fb1d0b78b
Arun <arun11.kb@gmail.com>
2024-04-04 13:09:44 +0530

    js for header scrolling  added

--- script.js
+++ script.js
@@ -9,13 +9,35 @@ const adjustBottomImgContainerSize = () => {
     const titleElement = bottomItemTitles[i]
     const lineHeight = titleElement.offsetHeight;
     if (lineHeight > maxHeight) {
-      console.log('line height exceeded')
-      console.log(titleElement)
       bottomItemImageContainers[i].style.height = 'auto';
     }
   }
 }
 
 
+const headerOnScroll = () => {
+  const header = document.querySelector('.header')
+  let prevScrollPos = window.scrollY
+  console.log(header)
+  window.addEventListener('scroll', () => {
+    const currentScrollPos = window.scrollY
 
+    console.log(currentScrollPos)
+    if (currentScrollPos < 324) {
+      header.classList.add('header-transparent')
+    } else if (prevScrollPos < currentScrollPos) {
+      header.classList.add('header-hide')
+    } else {
+      header.classList.remove('header-hide')
+      header.classList.remove('header-transparent')
+    }
+    prevScrollPos = currentScrollPos
+  })
+}
+
+
+// * header scroll
+headerOnScroll()
+
+// * adjusts image size 
 adjustBottomImgContainerSize()
